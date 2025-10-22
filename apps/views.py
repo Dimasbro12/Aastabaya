b54d28d508547cee8d4efae30b6de252b7b3a2e4
Dimasbro12 <dwid35895@gmail.com>
2025-10-22 11:15:57 +0700

    Setup Project Aastabaya

---
+++ apps/views.py
@@ -0,0 +1,9 @@
+from re import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def apps(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
